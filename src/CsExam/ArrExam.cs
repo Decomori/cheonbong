3a2b03bde561f4d256c907b3a71f91b7000d010e
김태성 <zmsqufxotjd@naver.com>
2019-08-25 07:51:30 +0000

    8.25 ArrExam

---
+++ src/CsExam/ArrExam.cs
@@ -0,0 +1,13 @@
+using System;
+namespace ConsoleApplication1
+	{
+	class Program
+{
+static void Main(string[] args)
+{
+int[] reading = new int[6] { 4, 9, 1, 0, 21, 12 };
+for (int i = 0; i < reading.Length; i++)
+Console.WriteLine("reading[" + i + "]={0}", reading[i]);
+}
+}
+}
